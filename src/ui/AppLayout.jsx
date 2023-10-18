b0aa6086f4e167346f9ed05e5a62989845f7c26f
Nguyen Huu Trong <trongnh2003@gmail.com>
2023-10-18 23:39:30 +0700

    Fix Bug Sidebar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,12 +5,12 @@ import Sidebar from './Sidebar';
 
 function AppLayout() {
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr] bg-[--color-grey-100]">
+    <div className="flex h-full flex-col bg-[--color-grey-100]">
       <Header />
-      <div className="flex justify-center">
+      <div className="mt-[52px] flex justify-center">
         <Sidebar />
         <main className="mx-[16px] mt-[40px] w-full min-w-[288px] max-w-[825px] bg-[--color-grey-50]">
-          <div className="mx-[auto] my-[0] flex max-w-[120rem] flex-col gap-[3.2rem]">
+          <div className="mx-[auto] my-[0] flex h-[2000px] max-w-[120rem] flex-col gap-[3.2rem]">
             <Outlet />
           </div>
         </main>
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,6 @@
 function Sidebar() {
   return (
-    <aside className="sticky top-[40px] mt-[40px] max-h-[90vh] w-[256px] bg-[--color-grey-50] px-[16px]">
+    <aside className="sticky top-[60px] mt-[40px] max-h-[80vh] w-[256px] bg-[--color-grey-50] px-[16px]">
       <h1>Side bar</h1>
     </aside>
   );
