de29489892c67c82be0120a16772111b2a3c6979
Nguyen Huu Trong <trongnh2003@gmail.com>
2023-10-18 23:08:34 +0700

    DarkLight Mode Toggle

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="col-span-full flex w-screen items-center border-b border-solid border-b-[--color-grey-300] bg-[--color-grey-50] px-[4.8rem] py-[1.2rem]">
+    <header className="col-span-full flex w-screen items-center border-b border-solid border-b-[--color-grey-300] bg-[--color-grey-0] px-[4.8rem] py-[1.2rem]">
       <h1>Header</h1>
     </header>
   );
