7176fabb5b7dccb2ed3b75610c6dc391830fcac1
ngvahiu <vanhieu230303@gmail.com>
2023-10-19 18:42:49 +0700

    update sidebar

--- src/features/Sidebar/sidebarSlice.js
+++ src/features/Sidebar/sidebarSlice.js
@@ -1,8 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+<<<<<<< HEAD
     activeTab: 'boards',
     showSubTemplateTabs: false
+=======
+    activeTab: 'boards'
+>>>>>>> 3adba71 (update sidebar)
 };
 
 const sidebarSlice = createSlice({
@@ -11,12 +15,19 @@ const sidebarSlice = createSlice({
     reducers: {
         setActiveTab(state, action) {
             state.activeTab = action.payload;
+<<<<<<< HEAD
         },
         setShowSubTemplateTabs(state, action) {
             state.showSubTemplateTabs = action.payload;
+=======
+>>>>>>> 3adba71 (update sidebar)
         }
     }
 });
 
+<<<<<<< HEAD
 export const { setActiveTab, setShowSubTemplateTabs } = sidebarSlice.actions;
+=======
+export const { setActiveTab } = sidebarSlice.actions;
+>>>>>>> 3adba71 (update sidebar)
 export default sidebarSlice.reducer;
