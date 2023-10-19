3adba71f41036f2eeddb15eb146f17e412df64ca
ngvahiu <vanhieu230303@gmail.com>
2023-10-19 18:42:49 +0700

    update sidebar

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import exampleSlice from './features/example/exampleSlice';
+import sidebarSlice from './features/Sidebar/sidebarSlice';
 
 const store = configureStore({
   reducer: {
     example: exampleSlice,
+    sidebar: sidebarSlice
   },
 });
 
