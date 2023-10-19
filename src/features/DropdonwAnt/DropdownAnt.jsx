58f1c21837a6afe3903ed94af988bd1e4b9f971b
ngvahiu <vanhieu230303@gmail.com>
2023-10-19 16:24:48 +0700

    update Header, DropdownAnt, index.css

--- src/features/DropdonwAnt/DropdownAnt.jsx
+++ src/features/DropdonwAnt/DropdownAnt.jsx
@@ -1,30 +1,48 @@
-import DropdownWorkspaces from './DropdownWorkspaces';
-import DropdownInfo from './DropdownInfo';
-import DropdownStarred from './DropdownStarred';
-import DropdownSample from './DropdownSample';
-import DropdownRecently from './DropdownRecently';
-import DropdownCreate from './DropdownCreate';
+import { Dropdown, Space } from 'antd';
 
-const DropdownAnt = ({ type, title, items }) => {
-  
-  const DropdownComponent =
-    type === 'workspaces' ? (
-      <DropdownWorkspaces items={items} title={title} />
-    ) : type === 'info' ? (
-      <DropdownInfo />
-    ) : type === 'starred' ? (
-      <DropdownStarred items={items} title={title} />
-    ) : type === 'sample' ? (
-      <DropdownSample items={items} title={title} />
-    ) : type === 'recently' ? (
-      <DropdownRecently items={items} title={title} />
-    ) : type === 'create' ? (
-      <DropdownCreate title={title} />
-    ) : (
-      <></>
-    );
+function DropdownAnt({ type, title, items }) {
+  const header =
+    'mr-1 flex h-11 rounded p-2 text-lg font-semibold text-[--color-text] hover:bg-[--color-grey-200]';
+  const info =
+    'h-12 w-12 ml-2 flex items-center justify-center rounded-full bg-[--color-dark] p-2 text-sm font-semibold text-white hover:bg-[--color-grey-300] hover:text-[--color-dark]';
 
-  return <>{DropdownComponent}</>;
-};
+  const menuProps = {
+    items: items.map((item, id) => {
+      return {
+        key: item.id,
+        label: (
+          <a href="/" className='flex justify-center items-center'>
+            <div className='flex-[1] object-contain'>
+              <img src={item.img} alt="image_alt" />
+            </div>
+            <div className='flex-[3]'>
+              <div>{item.title}</div>
+              <div>{item.des}</div>
+            </div>
+          </a>
+        ),
+      };
+    }),
+  };
+
+  return (
+    <>
+      <Dropdown menu={menuProps} trigger={['click']} arrow={true}>
+        <button
+          className={`${
+            type === 'info' ? info : header
+          } px-[1.5rem] py-[0.5rem] focus:bg-[--header-button-bg-hovered] focus:text-[--header-button-txt-hovered]`}
+        >
+          <span onClick={(e) => e.preventDefault()}>
+            <Space size='small' className='flex items-center text-[1.2rem]'>
+              {title}
+              {type === 'header' && <i className="fa fa-angle-down"></i>}
+            </Space>
+          </span>
+        </button>
+      </Dropdown>
+    </>
+  );
+}
 
 export default DropdownAnt;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 import DropdownAnt from '../features/DropdonwAnt/DropdownAnt';
+=======
+import { CgMenuGridR } from 'react-icons/cg';
+import DropdownAnt from '../features/DropdonwAnt/DropdownAnt';
+import { BellOutlined } from '@ant-design/icons';
+>>>>>>> 2a212d4 (update Header, DropdownAnt, index.css)
 import logo from '../assets/logo_blue.png';
 import { AiOutlineBell } from 'react-icons/ai';
 import { CgMenuGridR } from "react-icons/cg";
@@ -117,11 +123,11 @@ function Header({ color = 'primary', className = '' }) {
   return (
     <header className="fixed w-screen items-center border-b border-solid border-b-[--color-grey-300] bg-[--color-grey-50] z-10">
       <nav className="flex items-center justify-between px-[4.8rem] py-[1.2rem]">
-        <button className="flex h-11 w-16 items-center justify-center rounded hover:bg-[--color-grey-200] hover:text-[--header-button-color]">
+        {/* <button className="flex h-11 w-16 items-center justify-center rounded hover:bg-[--color-grey-200] hover:text-[--header-button-color]">
           <span className="flex  items-center justify-center leading-none">
             <CgMenuGridR size={24} color="color-[#445471]" />
           </span>
-        </button>
+        </button> */}
         <a
           href="/"
           className="relative block rounded px-2 hover:bg-[--color-grey-200] hover:text-[--header-button-txt-hovered]"
@@ -130,6 +136,7 @@ function Header({ color = 'primary', className = '' }) {
         </a>
         <div className="flex items-center basis-full">
           <div className="relative flex h-full shrink-0 flex-grow items-stretch">
+<<<<<<< HEAD
             <div className="mx-1 flex">
               <DropdownAnt
                 type={'workspaces'}
@@ -145,6 +152,13 @@ function Header({ color = 'primary', className = '' }) {
               <DropdownAnt type={'sample'} title={'Sample'} items={templateData} />
               <DropdownAnt type={'create'} title={'Create new'} />
               {/* <DropdownTest></DropdownTest> */}
+=======
+            <div className="mx-1 flex items-center gap-[1rem]">
+              <DropdownAnt type={'header'} title={'Workspaces'} items={items} />
+              <DropdownAnt type={'header'} title={'Recently'} items={items} />
+              <DropdownAnt type={'header'} title={'Starred'} items={items} />
+              <DropdownAnt type={'header'} title={'Sample'} items={items} />
+>>>>>>> 2a212d4 (update Header, DropdownAnt, index.css)
             </div>
           </div>
         </div>
