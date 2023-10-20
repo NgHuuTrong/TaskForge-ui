14555d54093216607d211d2af99898a0b55f4625
Toldblog <tophuochung@gmail.com>
2023-10-20 23:46:36 +0700

    update dropdown components

--- src/components/DropdonwAnt/DropdownAnt.jsx
+++ src/components/DropdonwAnt/DropdownAnt.jsx
@@ -1,52 +1,30 @@
-import { Dropdown, Space } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import DropdownItem from './DropdownItem';
+import DropdownWorkspaces from './DropdownWorkspaces';
+import DropdownInfo from './DropdownInfo';
+import DropdownStarred from './DropdownStarred';
+import DropdownSample from './DropdownSample';
+import DropdownRecently from './DropdownRecently';
+import DropdownCreate from './DropdownCreate';
 
 const DropdownAnt = ({ type, title, items }) => {
-  const header =
-    'mr-1 flex h-11 rounded p-2 text-lg font-semibold text-[--color-text] hover:bg-[--color-grey-200]';
-  const info =
-    'ml-2 flex h-9 w-9 items-center justify-center rounded-full bg-[--color-dark] p-2 text-sm font-semibold text-white hover:bg-[--color-grey-300] hover:text-[--color-dark]';
+  
+  const DropdownComponent =
+    type === 'workspaces' ? (
+      <DropdownWorkspaces items={items} title={title} />
+    ) : type === 'info' ? (
+      <DropdownInfo items={items} title={title} />
+    ) : type === 'starred' ? (
+      <DropdownStarred items={items} title={title} />
+    ) : type === 'sample' ? (
+      <DropdownSample items={items} title={title} />
+    ) : type === 'recently' ? (
+      <DropdownRecently items={items} title={title} />
+    ) : type === 'create' ? (
+      <DropdownCreate title={title} />
+    ) : (
+      <></>
+    );
 
-  const menuProps = {
-    items: items.map((item, id) => {
-      return {
-        key: item.id,
-        label: (
-          <a href="/" className='flex justify-center items-center'>
-            <div className='flex-[1] object-contain'>
-              <img src={item.img} alt="image_alt" />
-            </div>
-            <div className='flex-[3]'>
-              <div>{item.title}</div>
-              <div>{item.des}</div>
-            </div>
-          </a>
-        ),
-      };
-    }),
-  };
-
-  return (
-    <>
-      <Dropdown menu={menuProps} trigger={['click']} arrow={true}>
-        <button
-          className={`${
-            type === 'info' ? info : header
-          } focus:bg-[--header-button-bg-hovered] focus:text-[--header-button-txt-hovered]`}
-        >
-          <a onClick={(e) => e.preventDefault()}>
-            <Space size={'small'} className="">
-              {title}
-              {type === 'header' ? <DownOutlined /> : <></>}
-            </Space>
-          </a>
-        </button>
-      </Dropdown>
-    </>
-  );
+  return <>{DropdownComponent}</>;
 };
-
-// DropdownAnt.displayName = 'DropdownAnt';
 
 export default DropdownAnt;
