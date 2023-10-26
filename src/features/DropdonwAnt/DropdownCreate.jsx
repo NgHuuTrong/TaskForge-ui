a958fee5d2b82feece1b2458d99daa52695043c9
Toldblog <tophuochung@gmail.com>
2023-10-26 09:42:59 +0700

    modal app member

--- src/features/DropdonwAnt/DropdownCreate.jsx
+++ src/features/DropdonwAnt/DropdownCreate.jsx
@@ -3,17 +3,28 @@ import { Dropdown, Space } from 'antd';
 import { TbTemplate, TbTable } from 'react-icons/tb';
 import { MdOutlinePeopleOutline } from 'react-icons/md';
 import Modal from '../../ui/Modal';
-import workspacebg from '../../assets/workspacebg.png'
-
+import workspacebg from '../../assets/workspacebg.png';
 
 const DropdownCreate = ({ title }) => {
-  const [openModal, setOpenModal] = useState(false);
+  const [openModalWorkspace, setOpenModalWorkspace] = useState(false);
+  const [openModalAddMember, setOpenModalAddMember] = useState(false);
+
+  function handleClick(id) {
+    if (id === 3) {
+      setOpenModalWorkspace(true);
+    } else if (id === 2) {
+      setOpenModalAddMember(true);
+    }
+    // else if (id === 1)
+    else return;
+  }
+
   function CommonItem(item) {
-    const { icon, title, des } = item;
+    const { icon, title, des, id } = item;
     return (
       <div
         className="my-2 flex max-w-md flex-col justify-center"
-        onClick={() => setOpenModal(true)}
+        onClick={() => handleClick(id)}
       >
         <div className="flex flex-row items-center font-semibold">
           {icon}
@@ -41,7 +52,6 @@ const DropdownCreate = ({ title }) => {
       icon: <MdOutlinePeopleOutline />,
       title: 'Create a workspace',
       des: 'A workspace is a collection of boards and people. Use Workspaces to organize your company, support busy people, family or friends',
-      modal: <Modal />,
     },
   ];
 
@@ -56,9 +66,25 @@ const DropdownCreate = ({ title }) => {
 
   return (
     <>
+      {/* MODAL */}
       <div>
-        <Modal openModal={openModal} setOpenModal={setOpenModal} size={'large'} background={workspacebg} />
+        <Modal
+          openModal={openModalWorkspace}
+          setOpenModal={setOpenModalWorkspace}
+          size={'large'}
+        >
+          <ModalWorkspaceChildren background={workspacebg} />
+        </Modal>
+        <Modal
+          openModal={openModalAddMember}
+          setOpenModal={setOpenModalAddMember}
+          size={'small'}
+        >
+          <ModalAddMember />
+        </Modal>
       </div>
+
+      {/* DROPDOWN */}
       <Dropdown menu={menuProps} trigger={['click']} arrow={true}>
         <button
           className={
@@ -77,3 +103,106 @@ const DropdownCreate = ({ title }) => {
 };
 
 export default DropdownCreate;
+
+const ModalWorkspaceChildren = ({ background }) => {
+  const [workspaceName, setWorkspaceName] = useState('');
+  return (
+    <div className="flex w-full flex-row">
+      <div className="flex flex-col px-20 py-10">
+        <div className="mt-10 flex flex-col">
+          <div className="text-4xl font-medium">
+            Let&apos;s build a Workspace
+          </div>
+          <span className="text-xl text-[--color-grey-400]">
+            Increase your productivity by making it easy for everyone to access
+            boards in one location.
+          </span>
+        </div>
+        <div className="flex flex-col justify-evenly">
+          <div className="my-10">
+            <div className="text-lg">Workspace Name</div>
+            <div className="flex max-w-xl">
+              <input
+                type="text"
+                placeholder="Taco's company"
+                className="w-[30rem] rounded-md border-[0.2rem] p-2 text-base outline-none transition-colors duration-500 focus:border-[var(--header-button-txt-hovered)]"
+                value={workspaceName}
+                onChange={(e) => setWorkspaceName(e.target.value)}
+              />
+            </div>
+            <span className="text-base text-[--color-grey-400]">
+              This is the name of your company, group or organization.
+            </span>
+          </div>
+          <div className="my-auto">
+            <div className="text-lg">
+              Description Workspace
+              <span className="text-[--color-grey-400]"> Options</span>
+            </div>
+            <div className="flex max-w-xl">
+              <textarea
+                className="w-[30rem] rounded-md border-[0.2rem] p-2 text-base outline-none transition-colors duration-500 focus:border-[var(--header-button-txt-hovered)]"
+                cols="40"
+                rows="5"
+                placeholder="Our team organizes everything here"
+              ></textarea>
+            </div>
+          </div>
+          <div className="mt-10 flex flex-col">
+            <span className="text-base text-[--color-grey-400]">
+              Put your members on a board with a short description of your
+              Workspace.
+            </span>
+            <button
+              className={`h-16 w-[30rem] text-xl transition-colors duration-500 ${
+                workspaceName.length > 0
+                  ? ' bg-[--header-button-txt-hovered] text-white'
+                  : ' cursor-not-allowed bg-[--color-grey-100] text-[--color-grey-400]'
+              }`}
+            >
+              Continue
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`ml-auto flex h-full w-full bg-[url(${background})]`}
+        style={{
+          background: `url(${background})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></div>
+    </div>
+  );
+};
+
+const ModalAddMember = () => {
+  const [memberName, setMemberName] = useState('');
+
+  return (
+    <div className="relative mt-10 flex w-full flex-col px-8">
+      <div>Welcome to the Workspace</div>
+      <div className="mt-10 flex w-full">
+        <input
+          type="text"
+          placeholder="Email or name"
+          className={`flex rounded-md border-[0.2rem] p-2 text-base outline-none transition-colors  [transition:width_.5s_ease-in-out] focus:border-[var(--header-button-txt-hovered)] ${memberName.length > 0 ? ' w-[32rem] border-[--header-button-txt-hovered]' : ' w-full'}`}
+          value={memberName}
+          onChange={(e) => setMemberName(e.target.value)}
+        />
+
+          <button
+            className={`flex justify-center items-center absolute right-16 py-2 px-6 w-[10rem] text-xl text-white transition-colors duration-1000 rounded-lg ${
+              memberName.length > 0
+                ? ' bg-[--header-button-txt-hovered] text-white visible '
+                : ' invisible '
+            }`}
+          >
+            Continue
+          </button>
+      </div>
+    </div>
+  );
+};
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { AiOutlineClose } from 'react-icons/ai';
 
@@ -9,19 +9,15 @@ function Modal({
   title = 'Title',
   size = 'small',
   children = null,
-  background = '',
 }) {
   const dialogRef = useRef(null);
-  const [workspaceName, setWorkspaceName] = useState('');
-
-  // const buttonDisabled = 'bg-[--color-grey-100] text-[--color-grey-400] cursor-not-allowed'
 
   const sizes = {
     small: ' w-[50rem] h-[15rem]',
     medium: ' w-[75rem] h-[25rem]',
     large: ' w-[100rem] h-[50rem] min-h-[50rem]',
   };
-  const base = ` flex bg-white relative overflow-hidden`;
+  const base = 'flex bg-white relative overflow-hidden';
 
   const classModalContainer = base + sizes[size];
 
@@ -33,84 +29,26 @@ function Modal({
     }
   }, [openModal]);
 
-  const dialog = openModal && createPortal(
-    <dialog
-      ref={dialogRef}
-      className="top-50 left-50 -translate-x-50 -translate-y-50 fixed z-10 flex rounded-xl backdrop:bg-gray-800/50"
-    >
-      <div className={`${classModalContainer}`}>
-        <div className=" absolute left-[96%] flex">
-          <AiOutlineClose
-            color="black"
-            onClick={() => setOpenModal(false)}
-            className="m-8 mb-2 ml-auto h-11 w-11 cursor-pointer px-2 py-1"
-          />
-        </div>
-
-        <div className="flex flex-1 flex-col px-20 py-10">
-          <div className="mt-10 flex flex-col">
-            <div className="text-4xl font-medium">
-              Let's build a Workspace
-            </div>
-            <span className="text-xl text-[--color-grey-400]">
-              Increase your productivity by making it easy for everyone to
-              access boards in one location.
-            </span>
+  const dialog =
+    openModal &&
+    createPortal(
+      <dialog
+        ref={dialogRef}
+        className="top-50 left-50 -translate-x-50 -translate-y-50 fixed z-10 flex rounded-xl backdrop:bg-gray-800/50"
+      >
+        <div className={`${classModalContainer}`}>
+          <div className=" absolute -right-2 -top-2 z-10">
+            <AiOutlineClose
+              color="black"
+              onClick={() => setOpenModal(false)}
+              className="m-8 mb-2 ml-auto h-11 w-11 cursor-pointer px-2 py-1"
+            />
           </div>
-          <div className="flex flex-col justify-evenly">
-            <div className="my-10">
-              <div className="text-lg">Workspace Name</div>
-              <div className="flex max-w-xl">
-                <input
-                  type="text"
-                  placeholder="Taco's company"
-                  className="w-[30rem] rounded-md border-[0.2rem] p-2 text-base outline-none transition-colors duration-500 focus:border-[var(--header-button-txt-hovered)]"
-                  value={workspaceName}
-                  onChange={(e) => setWorkspaceName(e.target.value)}
-                />
-              </div>
-              <span className="text-base text-[--color-grey-400]">
-                This is the name of your company, group or organization.
-              </span>
-            </div>
-            <div className="my-auto">
-              <div className="text-lg">
-                Description Workspace
-                <span className="text-[--color-grey-400]"> Options</span>
-              </div>
-              <div className="flex max-w-xl">
-                <textarea
-                  className="w-[30rem] rounded-md border-[0.2rem] p-2 text-base outline-none transition-colors duration-500 focus:border-[var(--header-button-txt-hovered)]"
-                  cols="40"
-                  rows="5"
-                  placeholder="Our team organizes everything here"
-                ></textarea>
-              </div>
-            </div>
-            <div className="mt-10">
-              <span className="text-base text-[--color-grey-400]">
-                Put your members on a board with a short description of your
-                Workspace.
-              </span>
-              <button className={`h-16 w-[30rem] text-xl transition-colors duration-500 ${workspaceName.length > 0 ? ' bg-[--header-button-txt-hovered] text-white' : ' bg-[--color-grey-100] text-[--color-grey-400] cursor-not-allowed'}`}>
-                Continue
-              </button>
-            </div>
-          </div>
+          {children}
         </div>
-
-        <div
-          className={`ml-auto h-full w-full flex-1 bg-[url(${background})]`}
-          style={{
-            background: `url(${background})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-          }}
-        ></div>
-      </div>
-    </dialog>,
-    document.body
-  );
+      </dialog>,
+      document.body,
+    );
 
   return dialog;
 }
