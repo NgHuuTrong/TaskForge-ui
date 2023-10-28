95f7281930db3fe13f2e39c3e4d0a117da5bd3c9
Nguyen Huu Trong <trongnh2003@gmail.com>
2023-10-28 09:00:27 +0700

    Update Dropdown and Heading Components

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -3,11 +3,15 @@ function Heading({ as = 'h1', children, classNames = '' }) {
     h1: 'text-[3rem]',
     h2: 'text-[2.5rem]',
     h3: 'text-[2rem]',
-    h4: 'text-[1.75rem]',
-    h5: 'text-[1.3rem]',
+    h4: 'text-[1.7rem]',
+    h5: 'text-[1.4rem]',
   };
 
-  return <h1 className={`font-[700] leading-6 ${styles[as]} ${classNames}`}>{children}</h1>;
+  return (
+    <h1 className={`text-[--color-grey-600] font-[700] leading-6 ${styles[as]} ${classNames}`}>
+      {children}
+    </h1>
+  );
 }
 
 export default Heading;
--- src/ui/MyDropdown.jsx
+++ src/ui/MyDropdown.jsx
@@ -1,16 +1,25 @@
-import { Dropdown, Space } from 'antd';
-import Button from './Button';
+import { Dropdown } from 'antd';
 import { HiOutlineChevronDown } from 'react-icons/hi2';
 
-function MyDropdown({ render, title }) {
+function MyDropdown({
+  render,
+  title,
+  hasChevron = true,
+  placement = 'bottomLeft',
+  type = 'header',
+}) {
+  const base =
+    'border-[none] rounded-[--border-radius-sm] [box-shadow:var(--shadow-sm)] flex gap-[6px] items-center [transition:all_0.2s] py-[6px] px-[10px]';
+  const types = {
+    header: 'text-[--color-grey-500] bg-none hover:bg-[--color-grey-100]',
+    primary: 'text-[--color-grey-50] bg-[--color-brand-500] hover:bg-[--color-brand-600]',
+  };
   return (
-    <Dropdown trigger={['click']} dropdownRender={() => render}>
-      <Button size="small" type="icon" onClick={(e) => e.preventDefault()}>
-        <Space size={'small'} className="text-[1.4rem]">
-          <span className="text-[--color-grey-500]">{title}</span>
-          <HiOutlineChevronDown size={16} />
-        </Space>
-      </Button>
+    <Dropdown placement={placement} trigger={['click']} dropdownRender={() => render}>
+      <button className={`${base} ${types[type]}`}>
+        <span className="text-[1.4rem] font-medium leading-[2rem]">{title}</span>
+        {hasChevron && <HiOutlineChevronDown size={16} />}
+      </button>
     </Dropdown>
   );
 }
