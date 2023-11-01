c501333ac1bd3d63c053b2702a80b9c36bd85a0c
ggnohope <lamhoa9a1thcsttlp@gmail.com>
2023-11-02 00:26:43 +0700

    add CreateFromTemplate.jsx

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -1,12 +1,17 @@
 import ExampleComponent from '../features/example/ExampleComponent';
 import ExampleTest from '../features/example/ExampleTest';
 import CreateBoard from '../ui/CreateBoard';
+import CreateFromTemplate from '../ui/CreateFromTemplate';
+
+import { templates } from '../ui/CreateFromTemplate';
+
 function Example() {
   return (
     <div>
       {/* <ExampleComponent />
       <ExampleTest /> */}
       <CreateBoard/>
+      
     </div>
   );
 }
--- src/ui/CreateBoard.jsx
+++ src/ui/CreateBoard.jsx
@@ -3,7 +3,6 @@ import { IoIosArrowBack, IoMdClose, IoIosCheckmark, IoIosMore } from 'react-icon
 import { BsHandIndex, BsPeople } from 'react-icons/bs';
 import { AiOutlineLock } from 'react-icons/ai';
 import { Popover, Input, Select, Space, Tooltip } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
 
@@ -92,7 +91,7 @@ const workspace = [
   }
 ]
 
-function CreateBoard() {
+function CreateBoard(props) {
   const [selectedIndex, setSelectedIndex] = useState(1);
   const [selectedWorkspace, setSelectedWorkspace] = useState(workspace ? workspace[0].title : '');
   const [backgroundType, setBackgroundType] = useState('image');
@@ -167,11 +166,125 @@ function CreateBoard() {
     else setInputStatus('error')
   }
 
+  if (props.template != undefined)
+  return (
+    <div className='w-[304px] p-[12px] shadow-md border'>
+
+      <div onClick={() => { }} className='flex flex-row justify-between items-center'>
+        <div onClick={() => {}} className='p-[10px] rounded-[5px] hover:bg-[#dcdfe4] cursor-pointer'>
+          <IoIosArrowBack />
+        </div>
+        <div>
+          <span className='text-[#44546f] text-[14px] font-semibold'>
+            {props.template.title}
+          </span>
+        </div>
+        <div onClick={() => { }} className='p-[10px] rounded-[5px] hover:bg-[#dcdfe4] cursor-pointer'>
+          <IoMdClose />
+        </div>
+      </div>
+
+      <div className='flex gap-[8px] items-center mt-[8px]'>
+        <img className="w-[40px] h-[40px] rounded-[4px]" src={props.template.backgroundPath} alt="" />
+        <span className='text-[14px] leading-[20px] font-bold'>{props.template.title}</span>
+      </div>
+
+      <div className='mt-[16px]'>
+        <span className='text-[12px] font-bold leading-[16px]'>
+          Board title
+        </span>
+        <span className='text-[12px] font-bold leading-[16px] text-[red]'>*</span>
+        <Input status={inputStatus} value={titleBoard} onChange={handleChangeTextInput} />
+        <div className='flex flex-row items-center gap-[10px] py-[5px]'>
+          <BsHandIndex />
+          <p className='text-[14px]'>
+            Board title is required
+          </p>
+        </div>
+      </div>
+
+      <div className='mt-[8px]'>
+        <div>
+          <span className='text-[12px] font-bold leading-[16px]'>
+            Workspace
+          </span>
+        </div>
+        <div>
+          <Select
+            defaultValue={workspace != null ? workspace[0].title : ''}
+            style={{
+              width: '100%'
+            }}
+            onChange={(value) => setSelectedWorkspace(value)}
+            options={workspace.map((item) => {
+              return { value: item.title }
+            })}
+          />
+        </div>
+      </div>
+
+      <div className='mt-[8px]'>
+        <div>
+          <span className='text-[12px] font-bold leading-[16px]'>
+            Visibility
+          </span>
+        </div>
+        <div>
+          <Select
+            defaultValue='Private'
+            style={{
+              width: '100%'
+            }}
+            onChange={{}}
+            optionLabelProp='value'
+            options={[
+              {
+                value: 'Private', 
+                label:
+                <Tooltip title="Only board members can see and edit this board.">
+                  <Space>
+                    <AiOutlineLock/>
+                    <span>Private</span>
+                  </Space>
+                </Tooltip>
+              },
+              {
+                value: 'Workspace', 
+                label: 
+                <Tooltip title={`All members of ${selectedWorkspace} can see and edit this board`}>
+                  <Space>
+                    <BsPeople/>
+                    <span>Workspace</span>
+                  </Space>
+                </Tooltip>
+              }
+            ]}
+          />
+        </div>
+      </div>
+      
+      <div className='mt-[16px]'>
+        {titleBoard != '' ? 
+        <div onClick={() => {}} className='flex justify-center p-[5px] bg-[#0c66e4] rounded-[5px] hover:bg-[#0055cc] cursor-pointer'>
+          <span className='text-[14px] leading-[20px] text-white'>
+            Create
+          </span>
+        </div> :
+        <div onClick={() => {}} className='flex justify-center opacity-50 p-[5px] bg-[#f1f2f4] rounded-[5px]'>
+          <span className='text-[14px] leading-[20px]'>
+            Create
+          </span>
+        </div>}
+      </div>
+    </div>
+  )
+
   return (
     <div className='w-[304px] shadow-md border'>
+    <div className='p-[12px]'>
 
-      <div onClick={() => { }} className='flex flex-row justify-between items-center p-[10px]'>
-        <div onClick={() => {}} className='p-[10px] rounded-[5px] hover:bg-[#dcdfe4]'>
+      <div className='flex flex-row justify-between items-center'>
+        <div onClick={() => {}} className='p-[10px] rounded-[5px] hover:bg-[#dcdfe4] cursor-pointer'>
           <IoIosArrowBack />
         </div>
         <div>
@@ -179,133 +292,129 @@ function CreateBoard() {
             Create Board
           </span>
         </div>
-        <div onClick={() => { }} className='p-[10px] rounded-[5px] hover:bg-[#dcdfe4]'>
+        <div onClick={() => { }} className='p-[10px] rounded-[5px] hover:bg-[#dcdfe4] cursor-pointer'>
           <IoMdClose />
         </div>
       </div>
 
-      <div className='p-[12px]'>
-
-        <div className='flex justify-center'>
-          <div
-            className="flex justify-center w-[200px] h-[120px] bg-cover rounded-[3px]"
-            style={{
-              backgroundImage: backgroundType === 'image' ?
-                `url(${backgroundImage[selectedIndex - 1].backgroundPath})` :
-                `url(${backgroundColor[selectedIndex - 1].backgroundPath})`
-            }}
-          >
-            <img className='p-[10px]' src="https://trello.com/assets/14cda5dc635d1f13bc48.svg" alt="/" />
-          </div>
+      <div className='flex justify-center mt-[8px]'>
+        <div
+          className="flex justify-center w-[200px] h-[120px] bg-cover rounded-[3px]"
+          style={{
+            backgroundImage: backgroundType === 'image' ?
+              `url(${backgroundImage[selectedIndex - 1].backgroundPath})` :
+              `url(${backgroundColor[selectedIndex - 1].backgroundPath})`
+          }}
+        >
+          <img className='p-[10px]' src="https://trello.com/assets/14cda5dc635d1f13bc48.svg" alt="/" />
         </div>
-
-        <div className='flex flex-col gap-[8px] mt-[16px]'>
-          <span className='text-[12px] font-bold leading-[16px]'>
-            Background
-          </span>
-          <div className='flex flex-row justify-between'>
-            {renderBackgroundImageList()}
-          </div>
-          <div className='flex flex-row justify-between'>
-            {renderBackgroundColorList()}
-          </div>
-        </div>
-
-        <div className='mt-[16px]'>
-          <span className='text-[12px] font-bold leading-[16px]'>
-            Board title
-          </span>
-          <span className='text-[12px] font-bold leading-[16px] text-[red]'>*</span>
-          <Input status={inputStatus} value={titleBoard} onChange={handleChangeTextInput} />
-          <div className='flex flex-row items-center gap-[10px] py-[5px]'>
-            <BsHandIndex />
-            <p className='text-[14px]'>
-              Board title is required
-            </p>
-          </div>
-        </div>
-
-        <div className='mt-[8px]'>
-          <div>
-            <span className='text-[12px] font-bold leading-[16px]'>
-              Workspace
-            </span>
-          </div>
-          <div>
-            <Select
-              defaultValue={workspace != null ? workspace[0].title : ''}
-              style={{
-                width: '100%'
-              }}
-              onChange={(value) => setSelectedWorkspace(value)}
-              options={workspace.map((item) => {
-                return { value: item.title }
-              })}
-            />
-          </div>
-        </div>
-
-        <div className='mt-[8px]'>
-          <div>
-            <span className='text-[12px] font-bold leading-[16px]'>
-              Visibility
-            </span>
-          </div>
-          <div>
-            <Select
-              defaultValue='Private'
-              style={{
-                width: '100%'
-              }}
-              onChange={{}}
-              optionLabelProp='value'
-              options={[
-                {
-                  value: 'Private', 
-                  label:
-                  <Tooltip title="Only board members can see and edit this board.">
-                    <Space>
-                      <AiOutlineLock/>
-                      <span>Private</span>
-                    </Space>
-                  </Tooltip>
-                },
-                {
-                  value: 'Workspace', 
-                  label: 
-                  <Tooltip title={`All members of ${selectedWorkspace} can see and edit this board`}>
-                    <Space>
-                      <BsPeople/>
-                      <span>Workspace</span>
-                    </Space>
-                  </Tooltip>
-                }
-              ]}
-            />
-          </div>
-        </div>
-        
-        <div className='flex flex-col gap-[8px] mt-[16px]'>
-          {titleBoard != '' ? 
-          <div onClick={() => {}} className='flex justify-center p-[5px] bg-[#0c66e4] rounded-[5px] hover:bg-[#0055cc] cursor-pointer'>
-            <span className='text-[14px] leading-[20px] text-white'>
-              Create
-            </span>
-          </div> :
-          <div onClick={() => {}} className='flex justify-center opacity-50 p-[5px] bg-[#f1f2f4] rounded-[5px]'>
-            <span className='text-[14px] leading-[20px]'>
-              Create
-            </span>
-          </div>}
-          <div onClick={() => {}} className='flex justify-center p-[5px] bg-[#f1f2f4] rounded-[5px] hover:bg-[#dcdfe4] cursor-pointer'>
-            <span className='text-[14px] leading-[20px]'>
-              Start with a template
-            </span>
-          </div>
-        </div>
-        
-
       </div>
+
+      <div className='flex flex-col gap-[8px] mt-[16px]'>
+        <span className='text-[12px] font-bold leading-[16px]'>
+          Background
+        </span>
+        <div className='flex flex-row justify-between'>
+          {renderBackgroundImageList()}
+        </div>
+        <div className='flex flex-row justify-between'>
+          {renderBackgroundColorList()}
+        </div>
+      </div>
+
+      <div className='mt-[16px]'>
+        <span className='text-[12px] font-bold leading-[16px]'>
+          Board title
+        </span>
+        <span className='text-[12px] font-bold leading-[16px] text-[red]'>*</span>
+        <Input status={inputStatus} value={titleBoard} onChange={handleChangeTextInput} />
+        <div className='flex flex-row items-center gap-[10px] py-[5px]'>
+          <BsHandIndex />
+          <p className='text-[14px]'>
+            Board title is required
+          </p>
+        </div>
+      </div>
+
+      <div className='mt-[8px]'>
+        <div>
+          <span className='text-[12px] font-bold leading-[16px]'>
+            Workspace
+          </span>
+        </div>
+        <div>
+          <Select
+            defaultValue={workspace != null ? workspace[0].title : ''}
+            style={{
+              width: '100%'
+            }}
+            onChange={(value) => setSelectedWorkspace(value)}
+            options={workspace.map((item) => {
+              return { value: item.title }
+            })}
+          />
+        </div>
+      </div>
+
+      <div className='mt-[8px]'>
+        <div>
+          <span className='text-[12px] font-bold leading-[16px]'>
+            Visibility
+          </span>
+        </div>
+        <div>
+          <Select
+            defaultValue='Private'
+            style={{
+              width: '100%'
+            }}
+            onChange={{}}
+            optionLabelProp='value'
+            options={[
+              {
+                value: 'Private', 
+                label:
+                <Tooltip title="Only board members can see and edit this board.">
+                  <Space>
+                    <AiOutlineLock/>
+                    <span>Private</span>
+                  </Space>
+                </Tooltip>
+              },
+              {
+                value: 'Workspace', 
+                label: 
+                <Tooltip title={`All members of ${selectedWorkspace} can see and edit this board`}>
+                  <Space>
+                    <BsPeople/>
+                    <span>Workspace</span>
+                  </Space>
+                </Tooltip>
+              }
+            ]}
+          />
+        </div>
+      </div>
+      
+      <div className='flex flex-col gap-[8px] mt-[16px]'>
+        {titleBoard != '' ? 
+        <div onClick={() => {}} className='flex justify-center p-[5px] bg-[#0c66e4] rounded-[5px] hover:bg-[#0055cc] cursor-pointer'>
+          <span className='text-[14px] leading-[20px] text-white'>
+            Create
+          </span>
+        </div> :
+        <div onClick={() => {}} className='flex justify-center opacity-50 p-[5px] bg-[#f1f2f4] rounded-[5px]'>
+          <span className='text-[14px] leading-[20px]'>
+            Create
+          </span>
+        </div>}
+        <div onClick={() => {}} className='flex justify-center p-[5px] bg-[#f1f2f4] rounded-[5px] hover:bg-[#dcdfe4] cursor-pointer'>
+          <span className='text-[14px] leading-[20px]'>
+            Start with a template
+          </span>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
