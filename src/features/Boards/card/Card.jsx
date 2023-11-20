08b2a85be0f7a3ba4de919d918a7d8a2855e3dbd
Trần Hải Nam <steven.hai.ta856@gmail.com>
2023-11-20 11:50:40 +0700

    add icons to Card

--- src/features/Boards/card/Card.jsx
+++ src/features/Boards/card/Card.jsx
@@ -2,6 +2,8 @@ import { Button } from 'antd';
 import { useDispatch } from 'react-redux';
 import { deleteCard, setCurrentCardDetail } from '../boardSlice';
 import CardDetailMemberList from './CardDetail/CardDetailMemberList';
+import { BiMessageRounded } from 'react-icons/bi';
+import { MdAttachFile } from 'react-icons/md';
 
 function Card({ listId, card, index, setMoveCard, provided }) {
   const { id, userImage, description } = card;
@@ -24,8 +26,20 @@ function Card({ listId, card, index, setMoveCard, provided }) {
       }}
     >
       <p className="my-1 text-2xl">{description}</p>
-      <div className="flex w-full justify-end">
-        <CardDetailMemberList size={'small'}></CardDetailMemberList>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex">
+          <div className="mr-4 flex items-center">
+            <BiMessageRounded></BiMessageRounded>
+            <span className="ml-1 text-lg">1</span>
+          </div>
+          <div className="flex items-center">
+            <MdAttachFile></MdAttachFile>
+            <span className="ml-1 text-lg">1</span>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <CardDetailMemberList size={'small'}></CardDetailMemberList>
+        </div>
       </div>
       <div className="absolute left-full top-[1px] z-10 bg-white opacity-0 group-hover:opacity-100">
         <Button className="flex w-full items-center justify-center rounded-none border-none text-xl hover:bg-white">
