a99c0972cf7b9d8284110dffe3e281d36da169f2
Trần Hải Nam <steven.hai.ta856@gmail.com>
2024-01-08 10:03:25 +0700

    fix bug use login

--- src/features/Authenticate/useLogin.js
+++ src/features/Authenticate/useLogin.js
@@ -4,16 +4,16 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import authAxios, { getToken } from '../../utils/axios';
 
-export function useLogin({onSuccess}) {
+export function useLogin(data) {
+  const { onSuccess } = data ? { ...data } : { onSuccess: null };
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: () => {
       authAxios.defaults.headers.Authorization = `Bearer ${getToken()}`;
-      if(onSuccess) {
+      if (onSuccess) {
         onSuccess();
-      }
-      else {
+      } else {
         navigate('/boards', { replace: true });
       }
     },
